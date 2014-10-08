adb9bffb91567085158a666a854630859fef6178
SergeiKutanov <sergeikutanov@gmail.com>
2014-10-08 16:35:54 +0400

    private part of the site set up

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,14 +16,21 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+
+            //custom bundles
+            //enmash main bundle
             new Enmash\Bundle\PagesBundle\EnmashPagesBundle(),
+
+            //sonata bundles
             new Sonata\CoreBundle\SonataCoreBundle(),
             new Sonata\BlockBundle\SonataBlockBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Sonata\UserBundle\SonataUserBundle('FOSUserBundle'),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new \Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
-            new \Application\Sonata\UserBundle\ApplicationSonataUserBundle()
+            new \Application\Sonata\UserBundle\ApplicationSonataUserBundle(),
+            new \Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new \Sonata\AdminBundle\SonataAdminBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
