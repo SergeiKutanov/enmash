488d86ebba5424fae1075f2bb6baccf7106ef9cc
SergeiKutanov <sergeikutanov@gmail.com>
2014-12-03 07:52:01 +0300

    user fix

---
+++ src/Enmash/Bundle/UserControlBundle/Admin/UserAdmin.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Enmash\Bundle\UserControlBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class UserAdmin extends \Sonata\UserBundle\Admin\Model\UserAdmin
+{
+
+    /**
+     * @param FormMapper $formMapper
+     */
+}
