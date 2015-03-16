3dadf2d1197f52ed3b8d9b99d40bbf0d26f53e10
SergeiKutanov <sergeikutanov@gmail.com>
2015-03-16 13:27:44 +0300

    test controller clear

--- src/Enmash/Bundle/StoreBundle/Controller/TestController.php
+++ src/Enmash/Bundle/StoreBundle/Controller/TestController.php
@@ -21,40 +21,6 @@ class TestController extends Controller
      */
     public function runTestAction() {
 
-        /* @var $mm MediaManager */
-        $mm = $this->container->get('sonata.media.manager.media');
-
-        /* @var $galleryManager GalleryManager */
-        $galleryManager = $this->container->get('sonata.media.manager.gallery');
-
-        $em = $this->getDoctrine()->getManager();
-        /* @var $product Product */
-
-        $media = new Media();
-        $filePath = realpath('') . '/catalog/photo/С0108.jpg';
-        if (is_file($filePath)) {
-            $media->setBinaryContent($filePath);
-            $media->setContext('productimage');
-            $media->setProviderName('sonata.media.provider.image');
-            $mm->save($media);
-
-            $gallery = new Gallery();
-            $gallery->setName('test');
-            $gallery->setContext('productimage');
-            $gallery->setDefaultFormat('productimage_small');
-            $gallery->setEnabled(true);
-
-            $galleryHasMedia = new GalleryHasMedia();
-            $galleryHasMedia->setMedia($media);
-            $galleryHasMedia->setGallery($gallery);
-
-            $gallery->addGalleryHasMedias(
-                $galleryHasMedia
-            );
-            $galleryManager->save($gallery);
-        }
-//        $media->setBinaryContent()
-
         return new Response();
     }
 }
